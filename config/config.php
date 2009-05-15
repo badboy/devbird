64a3928273040637102e49e1e8ad1cb7b45679be
BadBoy_ <psp.badboy@gmail.com>
2009-05-16 01:42:30 +0200

    missing ; in config.php

--- config/config.php
+++ config/config.php
@@ -3,7 +3,7 @@ if(!defined('IN_CORE')) die('Direct Access is not allowed!');
 
 define('TABLE_PREFIX', 'db_');
 ##mysql data
-$mysql_hostname = ''
+$mysql_hostname = '';
 $mysql_username = '';
 $mysql_password = '';
 $mysql_database = '';
